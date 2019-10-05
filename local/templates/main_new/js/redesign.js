78ca79035be74bc2b7254388755a7c3b503dc6c7
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2019-10-05 17:49:21 +0300

    changes

--- local/templates/main_new/js/redesign.js
+++ local/templates/main_new/js/redesign.js
@@ -5,7 +5,39 @@ $(function(){
         dots: true,
         speed: 500,
         slidesToShow: 6,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 980,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 760,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 
     $('.popular-block-slider').slick({
